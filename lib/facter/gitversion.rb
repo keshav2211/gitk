e4cafc0e7e99c938050faaa2d875e0418436cee4
keshav <keshav.sharma@xoriant.com>
2015-08-21 19:46:08 +0530

    removed unnessary code

--- lib/facter/gitversion.rb
+++ lib/facter/gitversion.rb
@@ -2,11 +2,11 @@
 Facter.add('gitversion') do
   setcode do
     gitverstring = Facter::Core::Execution.exec('git --version')
-	if gitverstring.empty?
-		nil
-	else
+	if gitverstring
 		gitver=gitverstring.match(/\d+\.\d+\.\d+/)
 		gitver[0] if gitver
+	else
+		nil
 	end
   end
 end
