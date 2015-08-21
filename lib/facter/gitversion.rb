18e79b72d5bdf2c6d5b5cc2f0681a66f8f4ae32d
keshav <keshav.sharma@xoriant.com>
2015-08-21 19:39:10 +0530

    removed unnessary code

--- lib/facter/gitversion.rb
+++ lib/facter/gitversion.rb
@@ -1,35 +1,12 @@
 #use git binary
 Facter.add('gitversion') do
-  has_weight 150
   setcode do
-    Facter::Core::Execution.exec('git --version | tr -d "[:alpha:]|[:space:]"')
-  end
-end
-
-#use puppet resource tool if git binary doesn't work(eg.Windows)
-Facter.add('gitversion') do
-  has_weight 100
-  setcode do
-    gitver = Facter::Core::Execution.exec('puppet resource package git | grep version')
-	if gitver.empty?
+    gitverstring = Facter::Core::Execution.exec('git --version')
+	if gitverstring.empty?
 		nil
 	else
-		gitver.match(/\d+\.\d+\.\d+/)[0]
-	end
-  end
-end
-
-#check ensure attribute if version attribute not present
-Facter.add('gitversion') do
-  has_weight 50
-  setcode do
-    gitensure = Facter::Core::Execution.exec('puppet resource package git | grep ensure')
-	if gitensure.empty?
-		nil
-	else
-		gitver=gitensure.match(/\d+\.\d+\.\d+/)
+		gitver=gitverstring.match(/\d+\.\d+\.\d+/)
 		gitver[0] if gitver
 	end
   end
 end
-
