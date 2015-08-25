bdf3fb6a634e1eb12983adcbdbfc41e0f960571e
keshav <keshav.sharma@xoriant.com>
2015-08-25 17:32:56 +0530

    refactored

--- manifests/gitrepo.pp
+++ manifests/gitrepo.pp
@@ -1,5 +1,11 @@
 define gitk::gitrepo {
 
+file { '/opt/git':
+  ensure  => 'directory',
+  group   => 'git',
+  owner   => 'git',
+}
+
 exec { "$title":
   command => "git init --bare ${title}.git",
   cwd     => '/opt/git/',
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,37 +1,7 @@
 class gitk {
 
-user { 'git':
-  ensure           => 'present',
-  managehome       => 'true',
-  password         => '$6$3ToZK6Ea6$j9qhesMzwN7RQN780h78hVOsVqzhMfGYiGPG5YL8q6WYIe2oQwTim6Gu5j9Q89000ZB4fj1mDzZ3Qbs2FKbhn.',
-  password_max_age => '99999',
-  password_min_age => '0',
-  shell            => '/bin/bash',
-}
-
-file { '/home/git/.ssh':
-  ensure  => 'directory',
-  group   => 'git',
-  mode    => '0700',
-  owner   => 'git',
-}
-
-file { '/home/git/.ssh/authorized_keys':
-  ensure  => 'file',
-  group   => 'git',
-  mode    => '0600',
-  owner   => 'git',
-  source  => 'puppet:///modules/gitk/authorized_keys',
-}
-
 package { 'git':
   ensure => 'present',
-}
-
-file { '/opt/git':
-  ensure  => 'directory',
-  group   => 'git',
-  owner   => 'git',
 }
 
 }
